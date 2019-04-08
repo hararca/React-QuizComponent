d8aa2d78a4ce346bcbee5ea5f27e0d4629c8d6be
Héctor Rodríguez <hector.rodriguez@diconium.com>
2019-04-08 16:52:10 +0200

    Change again

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json');
 class Quiz extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { quiz_position: 1 };
+    this.state = { quiz_position: 3 };
   }
   render() {
     return (
